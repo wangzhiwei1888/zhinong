a5e333678687f290e39e222697b6b5f717aaece9
zhiweiwang6 <zhiweiwang6@creditease.cn>
2016-10-30 17:02:07 +0800

    fixbug

--- client/template/header.js
+++ client/template/header.js
@@ -2,5 +2,13 @@ Template.header.events({
   "click .logOut": function(event, template){
     console.log('asdfasd');
      Meteor.logout();
+  },
+  "click .Algro": function(event, template){
+    
+    $('document,body').animate({scrollTop: '719px'}, 800);
+  },
+  "click .list .pub": function(event, template){
+    
+    $('document,body').scrollTop(0);
   }
 });
