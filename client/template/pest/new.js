7a7a8ab85c89ff861c1946b4cdc859ddcf7d237c
zhiweiwang6 <zhiweiwang6@creditease.cn>
2016-11-07 10:45:04 +0800

    meteor add multiparty

--- client/template/pest/new.js
+++ client/template/pest/new.js
@@ -6,37 +6,37 @@ Template.pestNew.helpers({
 
 });
 
-Template.pestNew.events({
-  "click #cerateorgann": function (event, template) {
-    var options = {};
-    window.localStorage.setItem("pestId", "dsafsdfdsf");
-    options.situationId = window.localStorage.getItem("situationId")
-    options.type = $("select[name='pesttype']").val();
-    options.name = $("input[name='pestname']").val();
-    options.userId = Meteor.userId();
-    options.Leavrs = [1,2,3];
-    options.Flowers = [1,2,3];
-    options.Root = [];
-    options.Insects = [1,2,3];
-    options.Eggs = [];
-    options.Desease = [];
-    console.log('options', options);
-    console.log('sdfsd', options.type !== "0");
-    if (options.name && options.type !== "0" && options.situationId) {
-      Meteor.call("insertPest", options, function(error, result){
-        if(error){
-          console.log("error", error);
-        }
-        if(result){
-        }
-      });
-      Router.go('solutionnew');
-    }else {
-      alert("The information is incomplete");
-    }
-    // Router.go('solutionnew', {_id: 1}, {query: 'q=s', hash: 'hashFrag'});
-  }
-});
+// Template.pestNew.events({
+//   "click #cerateorgann": function (event, template) {
+//     var options = {};
+//     window.localStorage.setItem("pestId", "dsafsdfdsf");
+//     options.situationId = window.localStorage.getItem("situationId")
+//     options.type = $("select[name='pesttype']").val();
+//     options.name = $("input[name='pestname']").val();
+//     options.userId = Meteor.userId();
+//     options.Leavrs = [1,2,3];
+//     options.Flowers = [1,2,3];
+//     options.Root = [];
+//     options.Insects = [1,2,3];
+//     options.Eggs = [];
+//     options.Desease = [];
+//     console.log('options', options);
+//     console.log('sdfsd', options.type !== "0");
+//     if (options.name && options.type !== "0" && options.situationId) {
+//       Meteor.call("insertPest", options, function(error, result){
+//         if(error){
+//           console.log("error", error);
+//         }
+//         if(result){
+//         }
+//       });
+//       Router.go('solutionnew');
+//     }else {
+//       alert("The information is incomplete");
+//     }
+//     // Router.go('solutionnew', {_id: 1}, {query: 'q=s', hash: 'hashFrag'});
+//   }
+// });
 
 
 AutoForm.hooks({
---
+++ server/filesUpload.js
@@ -0,0 +1,32 @@
+var multiparty = require('multiparty');
+var util = require('util');
+
+Router.map(function() {
+
+	this.route('serverFile', {
+		where: 'server',
+		path: '/files',
+		action: function(req,res) {
+
+		    var form = new multiparty.Form();
+
+		    form.parse(req, function(err, fields, files) {
+		      if (err) {
+		        res.writeHead(400, {'content-type': 'text/plain'});
+		        res.end("invalid request: " + err.message);
+		        return;
+		      }
+		      // console.log(req)
+		      // console.log(req.files);
+		      res.writeHead(200, {'content-type': 'text/plain'});
+		      res.write('received fields:\n\n '+util.inspect(fields));
+		      res.write('\n\n');
+		      res.end('received files:\n\n '+util.inspect(files));
+
+		    })
+
+			
+		}
+	});
+
+})
