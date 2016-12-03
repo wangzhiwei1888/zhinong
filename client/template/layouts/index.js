eb3413195300cec72201b240d470de0f62e3a86f
zhiweiwang6 <zhiweiwang6@creditease.cn>
2016-12-03 18:41:22 +0800

    edit files

--- client/template/layouts/index.js
+++ client/template/layouts/index.js
@@ -2,13 +2,14 @@ Template.index.onRendered (function(){
   
   $('.footer-bg').css('background','none');
   $('.banner-bg-c').css('background','#fff');
-
+  $('.hideHeaderDiv').hide();
   
-
+  // window.location.href = window.location.href;
 });
 
 Template.index.onDestroyed (function(){
   
+  $('.hideHeaderDiv').show();
   $('.footer-bg').css('background','#f2f2f2');
   $('.banner-bg-c').css('background','#fdfdfd');
   
